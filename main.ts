fa2e7a138cbab808e50e1ef65345a87ea1877d9b
AlbionHusaj <150462649+AlbionHusaj@users.noreply.github.com>
2023-11-12 18:04:30 +0100

    Opdater pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,16 @@
 let NyLs = 0
 servos.P1.setAngle(0)
 let Ls = pins.analogReadPin(AnalogPin.P2)
+music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P10) == 1) {
-        NyLs = pins.analogReadPin(AnalogPin.P2)
-        if (NyLs < 0.9 * Ls) {
-            servos.P1.setAngle(90)
-            pins.digitalWritePin(DigitalPin.P0, 1)
-            basic.pause(3500)
-        } else {
-            Ls = (NyLs + Ls) / 2
-            servos.P1.setAngle(0)
-            pins.digitalWritePin(DigitalPin.P0, 0)
-        }
+    NyLs = pins.analogReadPin(AnalogPin.P2)
+    if (NyLs < 0.9 * Ls) {
+        servos.P1.setAngle(90)
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(3500)
     } else {
+        Ls = (NyLs + Ls) / 2
+        servos.P1.setAngle(0)
         pins.digitalWritePin(DigitalPin.P0, 0)
     }
 })
